e695208f366ee8f25a2c4eddf1a244b6f4d62b9c
Ali Raza <aliraza1567@gmail.com>
2020-02-24 23:10:13 +0100

    Add projects

---
+++ Books/Books.Domain/Entities/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Books.Domain.Entities
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Guid AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
---
+++ Books/Books.Persistance/Books/BookConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Books.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Books.Persistance.Books
+{
+    public class BookConfiguration : EntityTypeConfiguration<Book>
+    {
+        public BookConfiguration()
+        {
+            ToTable("Books");
+            HasKey(p => p.Id);
+            Property(p => p.Title).IsRequired().HasMaxLength(150);
+            Property(p => p.Description).IsRequired().HasMaxLength(2500);
+            HasRequired(p => p.Author);
+        }
+    }
+}
